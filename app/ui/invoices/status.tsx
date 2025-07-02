fd41978080dd53c92a82188e9e43de76a82c90d8
Victor Leon <vdleon30@gmail.com>
2025-07-02 11:49:07 -0400

    fix

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -3,9 +3,9 @@ import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
-    <span
+     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
         {
           'bg-gray-100 text-gray-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
